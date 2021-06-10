79e43985ef1718ac120bf9348fd5e5796f3cd978
Kaio Pomini <kaiopomini@gmail.com>
2021-06-10 14:19:53 -0300

    back-end: List items (get /items route)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import { Knex } from 'knex';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
+routes.get('/items', async (req, res) => {
 
-  return res.json({messege: 'Hello World'});
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return { 
+      title: item.title, 
+      image_url: `http://localhost:3333/uploads/${item.image}`, 
+    }
+  })
+
+  return res.json(serializedItems);
 });
 
 export default routes;
