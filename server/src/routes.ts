439eabf4ad14828cc6c69771096eaae13133a808
Kaio Pomini <kaiopomini@gmail.com>
2021-06-10 16:48:31 -0300

    back-end: post route to create a point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,13 +9,60 @@ routes.get('/items', async (req, res) => {
   const items = await knex('items').select('*');
 
   const serializedItems = items.map(item => {
-    return { 
-      title: item.title, 
-      image_url: `http://localhost:3333/uploads/${item.image}`, 
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
     }
   })
 
   return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body;
+
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items
+    .split(',')
+    .map((item: string) => Number(item.trim()))
+    .map((item_id: number) => {
+      return {
+        item_id,
+        point_id,
+      };
+    });
+
+  await trx('point_items').insert(pointItems);
+
+  await trx.commit();
+
+  return res.json({ success: true });
 });
 
 export default routes;
